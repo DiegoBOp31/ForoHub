c35d0c9c66d29f9f9f24b2eec83474493fe3f1cc
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-12 15:23:23 -0600

    Inicialización del proyecto

---
+++ src/test/java/com/porject/ForoHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porject.ForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
