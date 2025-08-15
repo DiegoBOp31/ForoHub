17378fedbd5042fb13f0e16e0e457a4d8628fd69
Diego Rosales Rebollar <diegorila-31@hotmail.com>
2025-08-15 15:45:20 -0600

    Creamos el repository de topico, agregamos configuraciones de la clase usuario y la clase topico

--- src/main/java/com/porject/ForoHub/domain/topico/Topico.java
+++ src/main/java/com/porject/ForoHub/domain/topico/Topico.java
@@ -1,15 +1,27 @@
 package com.porject.ForoHub.domain.topico;
 
-import com.porject.ForoHub.domain.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Getter//Crea todos los getters en tiempo de compilación
+@NoArgsConstructor//Crea un constructor sin argumentos
+@AllArgsConstructor//Crea un constructor con todos los argumentos
+@EqualsAndHashCode(of = "id")//El sistema identifica que dos objetos son iguales si tienen el mismo id
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
 public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
     private String mensaje;
     private LocalDateTime fechaCreacion;
     private Boolean status;
     private String curso;
-    private Usuario usuario;
 }
--- src/main/java/com/porject/ForoHub/domain/usuario/Usuario.java
+++ src/main/java/com/porject/ForoHub/domain/usuario/Usuario.java
@@ -1,4 +1,67 @@
 package com.porject.ForoHub.domain.usuario;
 
-public class Usuario {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter//Crea todos los getters en tiempo de compilación
+@NoArgsConstructor//Crea un constructor sin argumentos
+@AllArgsConstructor//Crea un constructor con todos los argumentos
+@EqualsAndHashCode(of = "id")//El sistema identifica que dos objetos son iguales si tienen el mismo id
+
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String contrasenia;
+
+    // Retorna la lista de roles/permisos del usuario.
+    // En este caso, asigna por defecto el rol "ROLE_USER" para control de acceso en Spring Security.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    // Métodos obligatorios al implementar UserDetails de Spring Security.
+    // Se usan para que el sistema conozca usuario, contraseña y estado de la cuenta.
+    @Override
+    public String getPassword() {
+        return contrasenia;
+    }// Contraseña del usuario
+
+    @Override
+    public String getUsername() {
+        return login;
+    }// Identificador o nombre de usuario
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }// La cuenta no está expirada
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }// La cuenta no está bloqueada
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }// La contraseña sigue vigente
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }// El usuario está habilitado
 }
